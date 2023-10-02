37a59d317a9f0ec8654126d10dbbe5faefc3928b
Anish Karthik <anishkarthik21@gmail.com>
2023-10-01 22:48:45 -0500

    Added internal links to self

--- cavs-algorhythms/pages/index.js
+++ cavs-algorhythms/pages/index.js
@@ -4,49 +4,52 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <meta name="description" content="Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms"></meta>
-        <meta name="keywords" content="Cav's AlgoRhythms, Cav's AlgoRhythms, Cav's AlgoRhythms, Cav's AlgoRhythms"></meta>
-        <meta name="author" content="Your Name"></meta>    
-        <title>Cav's AlgoRhythms</title>
-    </head>
-    <body>
-        <header>
-            <h1>Welcome to Cav's AlgoRhythms</h1>
-        </header>
-    
-        <section>
-            <h2>About Cav's AlgoRhythms</h2>
-            <p>SEO made by Anish Karthik UIN: 631004139</p>
-        </section>
-        <div>
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms
-        </div>
-    
-        <footer>
-            <p>&copy; 2023 Cav's AlgoRhythms. All rights reserved.</p>
-        </footer>
-    </body>
+      <title>Cav’s AlgoRhythms</title>
+      <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+          <meta name="description" content="Cav's AlgoRhythms"></meta>
+          <meta name="keywords" content="Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves"></meta>
+          <meta name="author" content="Your Name"></meta>    
+          <title>Cav’s AlgoRhythms</title>
+      </head>
+      <body>
+          <header>
+              <h1>Welcome to Cav's AlgoRhythms</h1>
+          </header>
+      
+          <section>
+              <h2>About Cav's AlgoRhythms</h2>
+              <p>SEO made by Anish Karthik UIN: 631004139</p>
+          </section>
+          <div>
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+            Cav's Algorithm, Cav's Algorithms, Cav's Algo Rhythms, Cav AlgoRhythms, Cav Algorithm, Cav's Algorithmic Solutions, Cav's Algorithmic Music, Algorithm Rhythms by Cav, Cav's Algorithmic Creations, Cav's Algorithmic Innovations, Cav's Algo Beats, Cav's Rhythmic Algorithms, Cav's Algorithmic Compositions, Cav's AlgoTunes, Cav's Algorithmic Patterns, Cav's Algorithmic Artistry, Cav's Algo Sounds, Cav's Rhythmic Computing, Cav's Algorithmic Designs, Cav's Algo Grooves
+          </div>
+      
+          <footer>
+              <p>&copy; 2023 Cav's AlgoRhythms. All rights reserved.</p>
+          </footer>
+      </body>
+      <div>Shamelessly link to own page</div>
+      <a href="https://cavs-algorhythms.vercel.app/" title="Cav’s AlgoRhythms">Cav’s AlgoRhythms</a>
+      <a href="https://cavs-algorhythms.vercel.app/" title="Cav’s AlgoRhythms">Cav’s AlgoRhythms</a>
+      <a href="https://cavs-algorhythms.vercel.app/" title="Cav’s AlgoRhythms">Cav’s AlgoRhythms</a>
+      <a href="https://cavs-algorhythms.vercel.app/" title="Cav’s AlgoRhythms">Cav’s AlgoRhythms</a>
+      <a href="https://cavs-algorhythms.vercel.app/" title="Cav’s AlgoRhythms">Cav’s AlgoRhythms</a>
+
     </div>
     );
 }
