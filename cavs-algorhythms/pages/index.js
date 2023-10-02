d8b1cedac95c8236b12536b87c77fc154916b9c9
Anish Karthik <anishkarthik21@gmail.com>
2023-10-01 22:37:23 -0500

    Added initial webpage with basic content

---
+++ cavs-algorhythms/pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="description" content="Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms Cav's AlgoRhythms"></meta>
+        <meta name="keywords" content="Cav's AlgoRhythms, Cav's AlgoRhythms, Cav's AlgoRhythms, Cav's AlgoRhythms"></meta>
+        <meta name="author" content="Your Name"></meta>    
+        <title>Cav's AlgoRhythms</title>
+    </head>
+    <body>
+        <header>
+            <h1>Welcome to Cav's AlgoRhythms</h1>
+        </header>
+    
+        <section>
+            <h2>About Cav's AlgoRhythms</h2>
+            <p>SEO made by Anish Karthik UIN: 631004139</p>
+        </section>
+    
+    
+        <footer>
+            <p>&copy; 2023 Cav's AlgoRhythms. All rights reserved.</p>
+        </footer>
+    </body>
+    </div>
+    );
+}
